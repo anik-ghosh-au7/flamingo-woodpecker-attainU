0a2b26bd1f8d420ee613ceddcca553a4f057471d
Anik Ghosh <tech.anikghosh@gmail.com>
2020-04-20 19:54:46 +0530

    solutions

--- solutions/rough.py
+++
@@ -1,9 +0,0 @@
-from functools import reduce
-
-lis = [1, 5, 4, 9, 8]
-
-max_elme = reduce(lambda a, b: a if a > b else b, lis)
-min_elme = reduce(lambda a, b: a if a < b else b, lis)
-
-print(max_elme)
-print(min_elme)
