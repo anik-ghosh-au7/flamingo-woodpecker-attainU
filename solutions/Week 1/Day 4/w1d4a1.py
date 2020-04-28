17ec6f1fb7a0d6914a0bb4697a92cc27d6efc2b7
Anik Ghosh <tech.anikghosh@gmail.com>
2020-04-28 11:20:01 +0530

    Questions update

--- solutions/Week 1/Day 4/w1d4a1.py
+++ solutions/Week 1/Day 4/w1d4a1.py
@@ -1,36 +1,36 @@
 # # Solution 1
 
 
-# def print_rangoli(size):
-#     value_a = ord('a')
-#
-#     for i in range(0, size):
-#
-#         print("-" * ((2 * (size - i)) - 2), end="")
-#         for j in range(0, i + 1):
-#             if j > 0:
-#                 print("-", end="")
-#             print(chr(value_a + (size - j - 1)), end="")
-#
-#         for k in range(i, 0, -1):
-#             print("-", end="")
-#             print(chr(value_a + (size - k)), end="")
-#
-#         print("-" * ((2 * (size - i)) - 2))
-#
-#     for i in range(size - 2, -1, -1):
-#
-#         print("-" * ((2 * (size - i)) - 2), end="")
-#         for j in range(0, i + 1):
-#             if j > 0:
-#                 print("-", end="")
-#             print(chr(value_a + (size - j - 1)), end="")
-#
-#         for k in range(i, 0, -1):
-#             print("-", end="")
-#             print(chr(value_a + (size - k)), end="")
-#
-#         print("-" * ((2 * (size - i)) - 2))
+def print_rangoli(size):
+    value_a = ord('a')
+
+    for i in range(0, size):
+
+        print("-" * ((2 * (size - i)) - 2), end="")
+        for j in range(0, i + 1):
+            if j > 0:
+                print("-", end="")
+            print(chr(value_a + (size - j - 1)), end="")
+
+        for k in range(i, 0, -1):
+            print("-", end="")
+            print(chr(value_a + (size - k)), end="")
+
+        print("-" * ((2 * (size - i)) - 2))
+
+    for i in range(size - 2, -1, -1):
+
+        print("-" * ((2 * (size - i)) - 2), end="")
+        for j in range(0, i + 1):
+            if j > 0:
+                print("-", end="")
+            print(chr(value_a + (size - j - 1)), end="")
+
+        for k in range(i, 0, -1):
+            print("-", end="")
+            print(chr(value_a + (size - k)), end="")
+
+        print("-" * ((2 * (size - i)) - 2))
 
 
 # Solution 2
