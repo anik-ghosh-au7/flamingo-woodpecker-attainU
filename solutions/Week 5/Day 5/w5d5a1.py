e3845c233062ddc3b4a8d9d8db9268f5afe3e221
Anik Ghosh <tech.anikghosh@gmail.com>
2020-05-10 23:22:50 +0530

    solutions

--- solutions/Week 5/Day 5/w5d5a1.py
+++ solutions/Week 5/Day 5/w5d5a1.py
@@ -1,66 +1,3 @@
-# # Node class
-# class Node:
-#     def __init__(self, data):
-#         self.data = data
-#         self.next = None
-#
-#
-# # Linked List class
-# class LinkedList:
-#     def __init__(self):
-#         self.head = None
-#         self.end = None
-#
-#     def append(self, data):
-#         new_node = Node(data)
-#         if self.head is None:
-#             self.head = new_node
-#             self.end = new_node
-#         else:
-#             self.end.next = new_node
-#             self.end = self.end.next
-#
-#     def reverse(self, k, c=0):  # Time complexity O(n)
-#
-#         prev_node = None
-#         curr_node = self.head
-#         count = 0
-#         if curr_node is None:
-#             return curr_node
-#         print('count', c)
-#         for _ in range(c):
-#             if curr_node.next is not None:
-#                 curr_node = curr_node.next
-#
-#         while curr_node is not None and count < k:
-#             next_node, curr_node.next = curr_node.next, prev_node
-#             prev_node, curr_node = curr_node, next_node
-#             count += 1
-#         c += k
-#         if next_node is not None:
-#             self.reverse(k, c)
-#         # self.end = self.head
-#         # self.head = prev_node
-#
-#     def display(self):
-#         curr_node = self.head
-#         while curr_node is not None:
-#             print('\033[46m', curr_node.data, '\033[41m', end=" --\033[0m-->")
-#             curr_node = curr_node.next
-#         else:
-#             print('\033[44m', "None", '\033[0m')
-#             print()
-#
-#
-# ll = LinkedList()
-# lis = [18, 5, 11, 30, 5]
-# for i in lis:
-#     ll.append(i)
-# ll.display()
-# ll.reverse(2)
-# ll.display()
-
-
 # Node class
 class Node:
     def __init__(self, data):
@@ -83,7 +20,6 @@ class LinkedList:
             self.end.next = new_node
             self.end = self.end.next
 
-
     def display(self):
         curr_node = self.head
         while curr_node is not None:
@@ -102,7 +38,6 @@ ll.display()
 
 
 def reverse(head, k):
-
     curr_node = head
     prev_node = None
 
