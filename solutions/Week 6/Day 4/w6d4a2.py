bcad2543525bad89bdd10b09c5f7fd9e5ca2b1cd
Anik Ghosh <tech.anikghosh@gmail.com>
2020-05-24 16:40:00 +0530

    solutions

--- solutions/Week 6/Day 4/w6d4a2.py
+++ solutions/Week 6/Day 4/w6d4a2.py
@@ -1,45 +1,30 @@
 class Graph:
-    def __init__(self, size, weighted=False, directed=False):
+    def __init__(self, size, directed=False):
         self.size = size
         self.graph = [[0 for _ in range(self.size)] for _ in range(self.size)]
-        self.weighted = weighted
         self.directed = directed
 
     def add_edges(self, u, v, w=0):
-        if self.weighted and self.directed:
+        if self.directed:
             self.graph[u][v] = w
-        elif self.weighted and not self.directed:
+        else:
             self.graph[u][v] = w
             self.graph[v][u] = w
-        elif not self.weighted and not self.directed:
-            self.graph[u][v] = 1
-            self.graph[v][u] = 1
-        else:
-            self.graph[u][v] = 1
 
     def get_elem(self, i, j):
         return self.graph[i][j]
 
-    def print_graph(self):
-        return self.graph
 
-
-def transform_to_adj_list(graph, weighted):
-    lis = {i: [] for i in range(graph.size)}
-    if weighted:
-        for i in range(graph.size):
-            for j in range(graph.size):
-                if graph.get_elem(i, j) != 0:
-                    lis[i].append((j, graph.get_elem(i, j)))
-    else:
-        for i in range(graph.size):
-            for j in range(graph.size):
-                if graph.get_elem(i, j) != 0:
-                    lis[i].append(j)
+def transform_to_adj_list(w_graph):
+    lis = {i: [] for i in range(w_graph.size)}
+    for i in range(w_graph.size):
+        for j in range(w_graph.size):
+            if w_graph.get_elem(i, j) != 0:
+                lis[i].append((j, w_graph.get_elem(i, j)))
     return lis
 
 
-g = Graph(5, True, False)
+g = Graph(5, False)
 g.add_edges(0, 1, 10)
 g.add_edges(1, 2, 20)
 g.add_edges(2, 3, 30)
@@ -47,12 +32,12 @@ g.add_edges(3, 0, 40)
 g.add_edges(1, 4, 50)
 
 print("Weighted & Undirected graph - ")
-print("matrix representation - ", g.print_graph())
-print("list representation - ", transform_to_adj_list(g, True))
+print("matrix representation - ", g.graph)
+print("list representation - ", transform_to_adj_list(g))
 
 print("----------------------------------------------------------------------------")
 
-g = Graph(5, True, True)
+g = Graph(5, True)
 g.add_edges(0, 1, 10)
 g.add_edges(1, 2, 20)
 g.add_edges(2, 3, 30)
@@ -60,8 +45,8 @@ g.add_edges(3, 0, 40)
 g.add_edges(1, 4, 50)
 
 print("Weighted & Directed graph - ")
-print("matrix representation - ", g.print_graph())
-print("list representation - ", transform_to_adj_list(g, True))
+print("matrix representation - ", g.graph)
+print("list representation - ", transform_to_adj_list(g))
 
 # Weighted & Undirected graph -
 # matrix representation -  [[0, 10, 0, 40, 0], [10, 0, 20, 0, 50], [0, 20, 0, 30, 0], [40, 0, 30, 0, 0], [0, 50, 0, 0, 0]]
--- solutions/Week 6/Day 4/w6d4cc1.py
+++ solutions/Week 6/Day 4/w6d4cc1.py
@@ -1,45 +1,30 @@
 class Graph:
-    def __init__(self, size, weighted=False, directed=False):
+    def __init__(self, size, directed=False):
         self.size = size
         self.graph = [[0 for _ in range(self.size)] for _ in range(self.size)]
-        self.weighted = weighted
         self.directed = directed
 
-    def add_edges(self, u, v, w=0):
-        if self.weighted and self.directed:
-            self.graph[u][v] = w
-        elif self.weighted and not self.directed:
-            self.graph[u][v] = w
-            self.graph[v][u] = w
-        elif not self.weighted and not self.directed:
+    def add_edges(self, u, v):
+        if self.directed:
             self.graph[u][v] = 1
-            self.graph[v][u] = 1
         else:
             self.graph[u][v] = 1
+            self.graph[v][u] = 1
 
     def get_elem(self, i, j):
         return self.graph[i][j]
 
-    def print_graph(self):
-        return self.graph
 
-
-def transform_to_adj_list(graph, weighted):
-    lis = {i: [] for i in range(graph.size)}
-    if weighted:
-        for i in range(graph.size):
-            for j in range(graph.size):
-                if graph.get_elem(i, j) != 0:
-                    lis[i].append((j, graph.get_elem(i, j)))
-    else:
-        for i in range(graph.size):
-            for j in range(graph.size):
-                if graph.get_elem(i, j) != 0:
-                    lis[i].append(j)
+def transform_to_adj_list(uw_graph):
+    lis = {i: [] for i in range(uw_graph.size)}
+    for i in range(uw_graph.size):
+        for j in range(uw_graph.size):
+            if uw_graph.get_elem(i, j) != 0:
+                lis[i].append(j)
     return lis
 
 
-g = Graph(5, False, False)
+g = Graph(5, False)
 g.add_edges(0, 1)
 g.add_edges(1, 2)
 g.add_edges(2, 3)
@@ -47,12 +32,12 @@ g.add_edges(3, 0)
 g.add_edges(1, 4)
 
 print("Unweighted & Undirected graph - ")
-print("matrix representation - ", g.print_graph())
-print("list representation - ", transform_to_adj_list(g, False))
+print("matrix representation - ", g.graph)
+print("list representation - ", transform_to_adj_list(g))
 
 print("----------------------------------------------------------------------------")
 
-g = Graph(5, False, True)
+g = Graph(5, True)
 g.add_edges(0, 1)
 g.add_edges(1, 2)
 g.add_edges(2, 3)
@@ -60,8 +45,8 @@ g.add_edges(3, 0)
 g.add_edges(1, 4)
 
 print("Unweighted & Directed graph - ")
-print("matrix representation - ", g.print_graph())
-print("list representation - ", transform_to_adj_list(g, False))
+print("matrix representation - ", g.graph)
+print("list representation - ", transform_to_adj_list(g))
 
 # Unweighted & Undirected graph -
 # matrix representation -  [[0, 1, 0, 1, 0], [1, 0, 1, 0, 1], [0, 1, 0, 1, 0], [1, 0, 1, 0, 0], [0, 1, 0, 0, 0]]
